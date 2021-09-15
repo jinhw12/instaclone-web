b079405fcb7be8002c7fda3b39ecdb15b9506a29
jinhw12 <jinhw12@naver.com>
2021-09-15 23:33:08 +0900

    setup with react-router-dom

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          {isLoggedIn ? <Home /> : <Login />}
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
